97c880791caaaf610b43decab640cacae31caf50
Tomas Miklusicak <tomas.miklusicak@uniqa.at>
2020-09-22 21:30:22 +0200

    Add namespace to cycle

--- java-version.sh
+++ java-version.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+NAMESPACE=$1
+
 if [ $# -eq 1 ]
   then
-    exit 0
+    echo -e "Checking project ${NAMESPACE}..."
 else
     echo -e "usage: ./java-version.sh <namespace>"
     exit 1
@@ -13,10 +15,10 @@ export STATUS=Running
 while read line 
 do
  EXITSTATUS=$?
- if [ ${EXITSTATUS} -gt 0 ]
+ if [ $EXITSTATUS -gt 0 ]
  sleep 1
   then
    echo -e "\033[1;32mcontainer:\033[m \033[4;31""m${line}\033[0m"
-   oc exec -t ${line} --namespace=${1} -- java -version | - 2> /dev/null
+   oc exec -t ${line} --namespace=${NAMESPACE} -- java -version | - 2> /dev/null
  fi
-done < <(oc get pods | grep -v NAME | awk -v LIMIT=${STATUS} '($3>=LIMIT) { print $1 }')
+done < <(oc get pods --namespace=${NAMESPACE} | grep -v NAME | awk -v LIMIT=${STATUS} '($3>=LIMIT) { print $1 }')
